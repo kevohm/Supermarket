090b8f30702efab95415fbf8379d8e9e05f6257e
kevohm <kevinkbet@gmail.com>
2025-04-21 09:21:31 +0300

    fix: linting issues

--- client/components/WeatherStats.tsx
+++ client/components/WeatherStats.tsx
@@ -7,12 +7,19 @@ interface WeatherStatsProps {
     unit: Unit;
   }
   
+  const formatWindspeed = (speed: number, unit:Unit): string => {
+    if (unit === "imperial") {
+      return `${Math.round(speed * 0.621371)}mph`;
+    }
+    return `${speed}km/h`
+  }
+
   export const WeatherStats = ({ windSpeed, windDirection, humidity, unit }: WeatherStatsProps) => (
     <div className="row-span-1 grid grid-cols-1 sm:grid-cols-2 gap-4">
       <div className="card bg-white shadow">
         <div className="card-body items-center text-center">
           <div className="text-gray-500">Wind Status</div>
-          <div className="text-2xl font-semibold">{windSpeed} {unit === "metric" ? "km/h" : "mph"}</div>
+          <div className="text-2xl font-semibold">{formatWindspeed(windSpeed, unit)}</div>
           <div className="text-sm text-gray-400">{windDirection}</div>
         </div>
       </div>
